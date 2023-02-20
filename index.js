cb4da2fb1d9dea2ffa796ee8bca4c54b60a69243
Nikedemos <phariseus@gmail.com>
2023-02-20 03:39:31 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const http = require('http');
+const fs = require('fs');
+
+let file = fs.createReadStream('loop.mp3');
+const port = 443;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'audio/mpeg' });
+  file.pipe(res);
+  res.on('close', () => {
+    file.destroy();
+    file = fs.createReadStream('loop.mp3');
+  });
+});
+
+server.listen(port);
+
+console.log(`Radio streaming at port {port}`);
